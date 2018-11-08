e4bfe7cb3013fe2f37e8d0e6013260295a93a91e
dunnl2236 <44269552+dunnl2236@users.noreply.github.com>
2018-11-08 12:08:05 -0500

    Add files via upload

---
+++ P2PRACTICE_Dunn.py
@@ -0,0 +1,18 @@
+# CTI110
+# P2PRACTICE_DUNN
+# Lacey Dunn
+# 11/08/2018
+# get input
+age=int(input("Enter age:"))
+# calclate the answer
+answer=age+1
+# print output
+# tell them how old they will be next year
+print("This is your age next year:", answer)
+
+
+
+
+
+           
+           
